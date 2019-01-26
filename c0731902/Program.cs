0db8a6ebf20ca09c242515e0fc7af94535c2b10c
shingarjeet singh <shingarsingh1997@gmail.com>
2019-01-25 21:03:38 -0500

    new villages

--- c0731902/Program.cs
+++ c0731902/Program.cs
@@ -5,20 +5,44 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace c0731902
-{
-    class Countryside
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class countryside
+    {
+        village Alst;
+        village Maeland;
+        village Schenig;
+        public void InitializeMap()
+        {
+            Alst = new village();
+            Maeland = new village();
+            Maeland.isAstrildHere = true;
+            village Schenig = new village();
+
+            Alst.NextVillage = Maeland;
+            Maeland.NextVillage = Schenig;
+            Schenig.NextVillage = Alst;
+        }
+        public void WalkAround()
+        {
+            village InitialVillage = this.Maeland;
+        }
 
     }
-
-
-    class Village
+    class village
     {
-        Village PreviousVillage;
-        Village NextVillage;
-        String VillageName;
-        bool isAstrildeHere = false;
-        public Village() { }
+        public village PreviousVillage;
+        public village NextVillage;
+        public string VillageName;
+        public bool isAstrildHere = false;
+        public village()
+        {
+
+        }
+
 
     }
-}
